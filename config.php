f3d391cd37b3acfb1bb48a7175108ddce797f13f
Ravi <ruchi.chauhan@bel-technology.com>
2016-04-26 16:10:47 +0530

    commit wellness project

--- config.php
+++ config.php
@@ -1,22 +1,22 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://localhost/upload/');
+define('HTTP_SERVER', 'http://localhost/wellness/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://localhost/upload/');
+define('HTTPS_SERVER', 'http://localhost/wellness/');
 
 // DIR
-define('DIR_APPLICATION', 'C:/wamp/www/upload/catalog/');
-define('DIR_SYSTEM', 'C:/wamp/www/upload/system/');
-define('DIR_LANGUAGE', 'C:/wamp/www/upload/catalog/language/');
-define('DIR_TEMPLATE', 'C:/wamp/www/upload/catalog/view/theme/');
-define('DIR_CONFIG', 'C:/wamp/www/upload/system/config/');
-define('DIR_IMAGE', 'C:/wamp/www/upload/image/');
-define('DIR_CACHE', 'C:/wamp/www/upload/system/cache/');
-define('DIR_DOWNLOAD', 'C:/wamp/www/upload/system/download/');
-define('DIR_UPLOAD', 'C:/wamp/www/upload/system/upload/');
-define('DIR_MODIFICATION', 'C:/wamp/www/upload/system/modification/');
-define('DIR_LOGS', 'C:/wamp/www/upload/system/logs/');
+define('DIR_APPLICATION', 'C:/wamp/www/wellness/catalog/');
+define('DIR_SYSTEM', 'C:/wamp/www/wellness/system/');
+define('DIR_LANGUAGE', 'C:/wamp/www/wellness/catalog/language/');
+define('DIR_TEMPLATE', 'C:/wamp/www/wellness/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/wamp/www/wellness/system/config/');
+define('DIR_IMAGE', 'C:/wamp/www/wellness/image/');
+define('DIR_CACHE', 'C:/wamp/www/wellness/system/cache/');
+define('DIR_DOWNLOAD', 'C:/wamp/www/wellness/system/download/');
+define('DIR_wellness', 'C:/wamp/www/wellness/system/wellness/');
+define('DIR_MODIFICATION', 'C:/wamp/www/wellness/system/modification/');
+define('DIR_LOGS', 'C:/wamp/www/wellness/system/logs/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
