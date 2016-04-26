9345f486e291606033e815d795e0fa45cdcdf048
Ravi <ruchi.chauhan@bel-technology.com>
2016-04-26 16:12:46 +0530

    commit

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/wellness/admin/');
+define('HTTP_CATALOG', 'http://localhost/wellness/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/wellness/admin/');
+define('HTTPS_CATALOG', 'http://localhost/wellness/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/wamp/www/wellness/admin/');
+define('DIR_SYSTEM', 'C:/wamp/www/wellness/system/');
+define('DIR_LANGUAGE', 'C:/wamp/www/wellness/admin/language/');
+define('DIR_TEMPLATE', 'C:/wamp/www/wellness/admin/view/template/');
+define('DIR_CONFIG', 'C:/wamp/www/wellness/system/config/');
+define('DIR_IMAGE', 'C:/wamp/www/wellness/image/');
+define('DIR_CACHE', 'C:/wamp/www/wellness/system/cache/');
+define('DIR_DOWNLOAD', 'C:/wamp/www/wellness/system/download/');
+define('DIR_wellness', 'C:/wamp/www/wellness/system/wellness/');
+define('DIR_LOGS', 'C:/wamp/www/wellness/system/logs/');
+define('DIR_MODIFICATION', 'C:/wamp/www/wellness/system/modification/');
+define('DIR_CATALOG', 'C:/wamp/www/wellness/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'admin');
+define('DB_PASSWORD', 'admin@123');
+define('DB_DATABASE', 'pawan');
+define('DB_PREFIX', 'oc_');
