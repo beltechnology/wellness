e46b60b3efe36c99ecdad9569a9d8e623cb645d5
beltechnology <swati.dashora@bel-technology.com>
2016-04-26 15:15:13 +0530

    Added files via upload
    commit

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/upload/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/wamp/www/upload/catalog/');
+define('DIR_SYSTEM', 'C:/wamp/www/upload/system/');
+define('DIR_LANGUAGE', 'C:/wamp/www/upload/catalog/language/');
+define('DIR_TEMPLATE', 'C:/wamp/www/upload/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/wamp/www/upload/system/config/');
+define('DIR_IMAGE', 'C:/wamp/www/upload/image/');
+define('DIR_CACHE', 'C:/wamp/www/upload/system/cache/');
+define('DIR_DOWNLOAD', 'C:/wamp/www/upload/system/download/');
+define('DIR_UPLOAD', 'C:/wamp/www/upload/system/upload/');
+define('DIR_MODIFICATION', 'C:/wamp/www/upload/system/modification/');
+define('DIR_LOGS', 'C:/wamp/www/upload/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'admin');
+define('DB_PASSWORD', 'admin@123');
+define('DB_DATABASE', 'pawan');
+define('DB_PREFIX', 'oc_');
